c6a15bec95b41583b5bf1dfcb82438025d76cffd
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-11 12:58:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,14 @@ class Student
     student
   end
 
+  def self.new_from_db(row)
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
+
 
 
 
